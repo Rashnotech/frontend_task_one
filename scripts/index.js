565eeaa3ec1cf4fe244e4e7d5036c954771fc5d1
Rashnotech <rashnotech@gmail.com>
2023-09-07 17:18:21 +0100

    Added Javascript the file

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+dt = new Date()
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurdsay", "Friday", "Saturday"]
+day = days[dt.getUTCDay()]
+time = dt.getTime()
+
+current_day = document.querySelector('.current_day')
+current_time = document.querySelector('.current_time')
+
+current_day.dataset.testid = day
+current_day.textContent = day
+current_time.dataset.testid = time
+current_time.textContent = time
